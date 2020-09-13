cfdd9643b28084cd0100f65ae9bb6c78d91b02b3
julian <ju851zel@htwg-konstanz.de>
2020-09-13 19:46:27 +0200

    added dependencies and cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-mod gateway;
+mod model;
+
+#[macro_use]
+extern crate bson;
+extern crate mongodb;
+
+mod dao;
+mod pagination;
 
 use actix_web::{App, HttpServer, web};
 use mongodb::Database;
-use mongodb::error::Error;
 
-use crate::recipe_routes::get_recipes;
-
-mod mongo_connector;
 mod recipe_routes;
 
 #[derive(Clone)]
@@ -16,21 +19,29 @@ pub struct AppState {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let database = match mongo_connector::start().await {
+    let database = match dao::init_database().await {
         Ok(db) => db,
-        Err(err) => {
-            panic!(err);
-        }
+        Err(err) => panic!(err)
     };
 
     let state = AppState { database };
 
+    let addr = "127.0.0.1:8088";
+
+    println!("Running on: {}", addr);
+
     HttpServer::new(move || {
         App::new()
             .data(state.clone())
-            .route("/recipes", web::get().to(get_recipes))
+            .service(
+                web::resource("/recipes")
+                    .name("recipes")
+                    .route(web::get().to(recipe_routes::get_recipes))
+                    .route(web::post().to(recipe_routes::add_recipe))
+            )
+        // .route("/recipes", web::get().to(get_recipes))
     })
-        .bind("127.0.0.1:8088")?
+        .bind(addr)?
         .run()
         .await
 }
---
+++ src/model/mod.rs
@@ -0,0 +1,4 @@
+pub mod recipe;
+pub mod ingredients;
+pub mod difficulty;
+pub mod measurement_unit;
